38fe6d1c89356223d6dbff1d152954c5fc178583
Aidan Pavlik <apavlik@hawk.iit.edu>
2026-04-17 16:11:45 -0500

    Add Initial Project Structure

---
+++ wealthmanagement/src/main/java/com/project2/wealthmanagement/WealthmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.project2.wealthmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthmanagementApplication.class, args);
+	}
+
+}
