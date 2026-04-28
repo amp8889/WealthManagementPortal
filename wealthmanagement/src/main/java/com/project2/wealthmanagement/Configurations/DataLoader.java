37b685cb4f6ddb11d362393f8f0a5206bd708c13
Aidan Pavlik <apavlik@hawk.iit.edu>
2026-04-28 15:12:53 -0500

    Create basic dataloader for clientRecords

--- wealthmanagement/src/main/java/com/project2/wealthmanagement/Configurations/DataLoader.java
+++ wealthmanagement/src/main/java/com/project2/wealthmanagement/Configurations/DataLoader.java
@@ -1,22 +1,25 @@
-// package com.project2.wealthmanagement.Configurations;
+package com.project2.wealthmanagement.Configurations;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.boot.CommandLineRunner;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-// import com.project2.wealthmanagement.Models.ClientRecords;
-// import com.project2.wealthmanagement.Repositories.ClientRecordsRepository;
+import com.project2.wealthmanagement.Enums.ClientTier;
+import com.project2.wealthmanagement.Enums.PrimaryObjective;
+import com.project2.wealthmanagement.Enums.RiskTolerance;
+import com.project2.wealthmanagement.Models.ClientRecords;
+import com.project2.wealthmanagement.Repositories.ClientRecordsRepository;
 
-// @Configuration
-// public class DataLoader {
+@Configuration
+public class DataLoader {
 
 
-//     @Bean
-//     CommandLineRunner init(ClientRecordsRepository repository){
-//         return args -> {
-//             repository.save(new ClientRecords("1", "Aidan", "Pavlik", "STANDARD", "USA", "AGGRESSIVE", "GROWTH", List.of("1", "2")));
-//         }
-//     }
-// }
+    @Bean
+    CommandLineRunner init(ClientRecordsRepository repository){
+        return args -> {
+            repository.save(new ClientRecords("1", "Aidan", "Pavlik", ClientTier.STANDARD, "USA", RiskTolerance.AGGRESSIVE, PrimaryObjective.BALANCED, List.of("1", "2")));
+        };
+    }
+}
