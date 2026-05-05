d2850df7211552603e6b97f674591d6be704469e
Aidan Pavlik <apavlik@hawk.iit.edu>
2026-05-05 16:17:02 -0500

    Add support for multiple goals

--- WealthManagementFrontEnd/src/app/client-dashboard/client-dashboard.ts
+++ WealthManagementFrontEnd/src/app/client-dashboard/client-dashboard.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, signal } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, FormArray, FormControl } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { ClientRecord } from '../types/ClientRecord';
@@ -11,6 +11,7 @@ import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { DatePicker } from 'primeng/datepicker';
 import { ProgressBarModule } from 'primeng/progressbar';
+
 @Component({
   selector: 'app-client-dashboard',
   standalone: true,
@@ -20,7 +21,8 @@ import { ProgressBarModule } from 'primeng/progressbar';
     InputTextModule,
     ButtonModule,
     DatePicker,
-    ProgressBarModule
+    ProgressBarModule,
+
   ],
   templateUrl: './client-dashboard.html',
   styleUrl: './client-dashboard.css',
@@ -28,10 +30,8 @@ import { ProgressBarModule } from 'primeng/progressbar';
 export class ClientDashboard implements OnInit {
 
   client = signal<ClientRecord | null>(null);
-  goal = signal<Goal | null>(null);
+  goals = signal<Goal[]>([]);
 
-
-  //This will need to be replaced when we get auth fully working with frontend
   clientId = "fa26bfc7-76fc-49d0-b99d-681f64a3438b";
 
   form!: FormGroup;
@@ -40,60 +40,77 @@ export class ClientDashboard implements OnInit {
     private goalService: GoalService,
     private clientService: ClientRecordsService,
     private formBuilder: FormBuilder
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-
     this.form = this.formBuilder.group({
-      currentSavedAmount: [''],
-
+      goals: this.formBuilder.array([])
     });
 
     this.loadClient();
-    this.loadGoal();
-
-    this.form.valueChanges.subscribe(val => {
-      console.log("FORM:", val);
-    });
+    this.loadGoals();
   }
 
   loadClient() {
     this.clientService.getById(this.clientId).subscribe({
-      next: (data) => {
-        this.client.set(data);
-      },
+      next: (data) => this.client.set(data),
       error: (err) => console.error(err)
     });
   }
 
-  loadGoal() {
-    this.goalService.getAll().subscribe(goals => {
-      const goal = goals.find(g => g.clientId === this.clientId);
 
-      if (goal) {
-        this.goal.set(goal);
-        this.form.patchValue({
-          currentSavedAmount: goal.currentSavedAmount
-        });
-      }
+getGoalGroup(index: number): FormGroup {
+  return this.formArray.at(index) as FormGroup;
+}
+
+
+  loadGoals() {
+    this.goalService.getAll().subscribe(goals => {
+const filtered = goals.filter(g =>
+  Array.isArray(g.clientId)
+    ? g.clientId.includes(this.clientId)
+    : g.clientId === this.clientId
+); 
+
+
+console.log("ALL GOALS FROM BACKEND:", goals);
+
+      this.goals.set(filtered);
+
+      const formArray = this.formArray;
+
+      formArray.clear();
+
+      filtered.forEach(goal => {
+        formArray.push(
+          this.formBuilder.group({
+            id: [goal.id],
+            currentSavedAmount: [goal.currentSavedAmount]
+          })
+        );
+      });
     });
   }
 
-saveGoal() {
-  const goal = this.goal();
+  get formArray(): FormArray {
+    return this.form.get('goals') as FormArray;
+  }
 
-  if (this.form.invalid || !goal) return;
+  updateGoal(index: number) {
+    const goal = this.goals()[index];
+    const control = this.formArray.at(index);
 
-  const updatedGoal = {
-    ...goal,
-    currentSavedAmount: this.form.value.currentSavedAmount
-  };
+    const updatedGoal: Goal = {
+      ...goal,
+      currentSavedAmount: control.value.currentSavedAmount
+    };
 
-  this.goalService.update(goal.id!, updatedGoal).subscribe({
-    next: (res) => {
-      this.goal.set(res);
-    }
-  });
-}
-
+    this.goalService.update(goal.id!, updatedGoal).subscribe({
+      next: (res) => {
+        this.goals.update(list =>
+          list.map(g => g.id === res.id ? res : g)
+        );
+      }
+    });
+  }
 }
