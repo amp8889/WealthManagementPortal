3441c4dff3c5b3838b2c959e94e74dc978223e23
Minh Pham <minhs2@yahoo.com>
2026-05-05 13:37:38 -0700

    Removed empty dashboard. Look to use Spring Security authorization instead

--- WealthManagementFrontEnd/src/app/advisor-dashboard/advisor-dashboard.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-advisor-dashboard',
-  imports: [],
-  templateUrl: './advisor-dashboard.html',
-  styleUrl: './advisor-dashboard.css',
-})
-export class AdvisorDashboard {}
