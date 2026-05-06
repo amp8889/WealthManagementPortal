6d5a435162751ed783ddde5683dca67c438d4bb1
Aidan Pavlik <apavlik@hawk.iit.edu>
2026-05-06 16:44:14 -0500

    Clean up merge request

--- WealthManagementFrontEnd/src/app/app.routes.ts
+++ WealthManagementFrontEnd/src/app/app.routes.ts
@@ -8,11 +8,11 @@ import { Register } from './register/register';
 export const routes: Routes = [
 
     { path: "client-records", component: ClientRecords },
-    {path: "goal", component: Goal},
-    {path: "home", component: Home},
-    {path: 'client-records/:id', component: ClientDashboard},
+    { path: "goal", component: Goal },
+    { path: "home", component: Home },
+    { path: 'client-records/:id', component: ClientDashboard },
     { path: "register", component: Register },
-        {
+    {
         path: 'dashboard/:clientId',
         loadComponent: () => import('./client-dashboard/client-dashboard').then(m => m.ClientDashboard),
     },
