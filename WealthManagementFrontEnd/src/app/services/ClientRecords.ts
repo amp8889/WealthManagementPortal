4d8f9959a4ec2c7b68d5a8f7cee517c287f226e9
Aidan Pavlik <apavlik@hawk.iit.edu>
2026-05-07 09:35:02 -0500

    Delete local env since it only seems to be pulling that one

--- WealthManagementFrontEnd/src/app/services/ClientRecords.ts
+++ WealthManagementFrontEnd/src/app/services/ClientRecords.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ClientRecord } from '../types/ClientRecord';
 import { Observable } from 'rxjs';
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
