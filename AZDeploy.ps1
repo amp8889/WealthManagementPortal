f7c4ea624183b4b6311fef161b92965b05f30787
Minh Pham <minhs2@yahoo.com>
2026-05-08 16:17:06 -0700

    Deployable to Azure

--- AZDeploy.ps1
+++ AZDeploy.ps1
@@ -5,11 +5,11 @@ cd ..
 Remove-Item -Path ".\wealthmanagement\src\main\resources\static\" -Recurse -Force
 Copy-Item -Path ".\WealthManagementFrontEnd\dist\WealthManagementFrontEnd\browser\*" -Destination ".\wealthmanagement\src\main\resources\static\" -Recurse -Force
 
-docker build --no-cache -t wealthmanagement:v9 .
-docker tag wealthmanagement:v9 apmpacr.azurecr.io/wealthmanagement:v9
+docker build --no-cache -t wealthmanagement:v11 .
+docker tag wealthmanagement:v11 apmpacr.azurecr.io/wealthmanagement:v11
 
 az acr login --name apmpacr
-docker push apmpacr.azurecr.io/wealthmanagement:v9
+docker push apmpacr.azurecr.io/wealthmanagement:v11
 
 az aks get-credentials --resource-group 20260316-EY-Java --name apmp-aks --overwrite-existing
 
@@ -17,5 +17,7 @@ kubectl apply -f .\azure\secret.yaml
 kubectl apply -f .\azure\deployment.yaml
 kubectl apply -f .\azure\service.yaml
 kubectl apply -f .\azure\ingress.yaml
+
+kubectl annotate svc -n app-routing-system nginx service.beta.kubernetes.io/azure-dns-label-name="wealthmanagementportal"
 
 kubectl rollout restart deployment/wealthmanagement
