b56eee0a55ace3790b375f15741cc9add37b49d8
Minh Pham <minhs2@yahoo.com>
2026-05-07 18:22:16 -0700

    Removed oauth2 dependency, v6 container tag

--- wealthmanagement/src/main/java/com/project2/wealthmanagement/Controllers/UserController.java
+++ wealthmanagement/src/main/java/com/project2/wealthmanagement/Controllers/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
