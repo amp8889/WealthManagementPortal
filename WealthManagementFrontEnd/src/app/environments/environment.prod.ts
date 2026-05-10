4a3bbcc7cd2ec8f148de54462933660c08496882
Aidan Pavlik <apavlik@hawk.iit.edu>
2026-05-09 20:49:08 -0500

    Update environment.prod.ts

--- WealthManagementFrontEnd/src/app/environments/environment.prod.ts
+++ WealthManagementFrontEnd/src/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://wealthmanagementportal.eastus.cloudapp.azure.com'
+  apiUrl: 'https://wealthmanagementportal.eastus.cloudapp.azure.com'
 };
